2248f883cb343b573d0366374f48c9f572b89654
Youngseok Choi <me@youngseok.com>
2026-03-26 07:52:54 +0900

    Bump version -> 0.4.1

--- src/pyadf/__init__.py
+++ src/pyadf/__init__.py
@@ -24,7 +24,7 @@ from .markdown import MarkdownConfig
 if TYPE_CHECKING:
     from typing import BinaryIO
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __all__ = [
     "Document",
     "MarkdownConfig",
